478c9274bb69d7b71afd9893403a63fb1644bf43
Andreas Kaiafas <akaiafas526@gmail.com>
2022-03-03 16:24:07 -0500

    set local storage for initials input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,32 @@
 // When start button is clicked, remove timer and diplay question 1
 
-// start btn needs event listener 
+// start btn needs event listener
 // hide start btn
 // create element to show timer and then append that element to the div container
 // same thing for question 1
 
 let data = [
-    {
-        question: 'What sport wears shin guards?',
-        options: [
-            'Basketball',
-            'Soccer',
-            'Baseball',
-            'Ping Pong',
-        ],
-        answer: 'Soccer'
-
-    },
-    {
-        question: 'What sport is played on ice?',
-        options: [
-            'Soccer',
-            'Baseball',
-            'Hockey',
-            'Cricket',
-        ],
-        answer: 'Hockey'
-
-    },
-    {
-        question: 'How many innings are played in a baseball game?',
-        options: [
-            'Six',
-            'Eight',
-            'Nine',
-            'Two',
-        ],
-        answer: 'Nine'
-    },
-    {
-        question: 'How many points is a touchdown in football?',
-        options: [
-            'Three',
-            'Six',
-            'Seven',
-            'Two',
-        ],
-        answer:
-            'Six'
-
-    }
-]
+  {
+    question: "What sport wears shin guards?",
+    options: ["Basketball", "Soccer", "Baseball", "Ping Pong"],
+    answer: "Soccer",
+  },
+  {
+    question: "What sport is played on ice?",
+    options: ["Soccer", "Baseball", "Hockey", "Cricket"],
+    answer: "Hockey",
+  },
+  {
+    question: "How many innings are played in a baseball game?",
+    options: ["Six", "Eight", "Nine", "Two"],
+    answer: "Nine",
+  },
+  {
+    question: "How many points is a touchdown in football?",
+    options: ["Three", "Six", "Seven", "Two"],
+    answer: "Six",
+  },
+];
 
 let startButton = document.getElementById("start-btn");
 let quizContent = document.getElementById("quiz-content");
@@ -60,34 +36,31 @@ let timer = document.createElement("h3");
 let secondsLeft = 60;
 let quizIndex = 0;
 let userScore = 0;
-let intervalId
+let intervalId;
 
 startButton.addEventListener("click", function (event) {
-    startButton.setAttribute("style", "display: none");
+  startButton.setAttribute("style", "display: none");
 
-    timer.textContent = secondsLeft;
-    quizContent.append(timer);
+  timer.textContent = secondsLeft;
+  quizContent.append(timer);
 
-    startTimer();
-    nextQuestion();
-})
+  startTimer();
+  nextQuestion();
+});
 
 function startTimer() {
-    intervalId = setInterval(() => {
-        secondsLeft--;
-        timer.textContent = secondsLeft;
-        if (secondsLeft <= 0) {
-            clearInterval(intervalId)
-        }
-    }, 1000);
-
-
-
+  intervalId = setInterval(() => {
+    secondsLeft--;
+    timer.textContent = secondsLeft;
+    if (secondsLeft <= 0) {
+      clearInterval(intervalId);
+    }
+  }, 1000);
 }
 
 function nextQuestion() {
-    let newDiv = document.createElement('div');
-    newDiv.innerHTML = `<h3>${data[quizIndex].question}</h3>
+  let newDiv = document.createElement("div");
+  newDiv.innerHTML = `<h3>${data[quizIndex].question}</h3>
     <div class="q-options">
         <button class="option-btns">${data[quizIndex].options[0]}</button>
         <button class="option-btns">${data[quizIndex].options[1]}</button>
@@ -95,63 +68,93 @@ function nextQuestion() {
         <button class="option-btns">${data[quizIndex].options[3]}</button>
     </div>`;
 
-    quizContent.append(newDiv);
+  quizContent.append(newDiv);
 
-    let allOptions = document.querySelectorAll(".option-btns");
+  let allOptions = document.querySelectorAll(".option-btns");
 
-    for (let option of allOptions) {
-        console.log(option);
+  for (let option of allOptions) {
+    console.log(option);
 
-        option.addEventListener("click", function (event) {
-            let selection = event.target.textContent;
-            console.log(selection)
-            if (selection === data[quizIndex].answer) {
-                console.log('Correct!')
-                userScore++;
-            } else {
-                selection.textContent = "Wrong!"
-                secondsLeft -= 10;
-            }
+    option.addEventListener("click", function (event) {
+      let selection = event.target.textContent;
+      console.log(selection);
+      if (selection === data[quizIndex].answer) {
+        console.log("Correct!");
+        userScore++;
+      } else {
+        selection.textContent = "Wrong!";
+        secondsLeft -= 10;
+      }
 
-            quizIndex++;
-            newDiv.remove();
+      quizIndex++;
+      newDiv.remove();
 
-            console.log(typeof quizIndex);
-            console.log(typeof data.length);
+      console.log(typeof quizIndex);
+      console.log(typeof data.length);
 
-            if (quizIndex === data.length) {
-                endGame();
-            } else {
-                nextQuestion();
-            }
-        })
-    }
+      if (quizIndex === data.length) {
+        endGame();
+      } else {
+        nextQuestion();
+      }
+    });
+  }
 }
 
 function endGame() {
-    clearInterval(intervalId)
-    timer.remove();
+  clearInterval(intervalId);
+  timer.remove();
 
-    let finalScore = document.createElement("p");
-    finalScore.textContent = userScore;
+  let finalScore = document.createElement("p");
+  finalScore.textContent = userScore;
 
-    let saveBtn = document.createElement("button");
-    saveBtn.textContent = "Save Score";
+  let saveBtn = document.createElement("button");
+  saveBtn.textContent = "Save Score";
 
-    quizContent.append(finalScore);
-    quizContent.append(saveBtn);
+  quizContent.append(finalScore);
+  quizContent.append(saveBtn);
 
-    saveBtn.addEventListener("click", function () {
-        saveBtn.remove();
-        setInitials();
-    })
+  saveBtn.addEventListener("click", function () {
+    saveBtn.remove();
+    setInitials(finalScore);
+  });
 }
 
+function setInitials(finalScore) {
+  let initials = "";
+  let newDiv = document.createElement("div");
+  newDiv.innerHTML = `<input placeholder='Enter Initials' type='text' id='initials'>
+  <button id='initial-submit'>Submit</button>`;
 
-function setInitials() {
-    let initials = []
-    localStorage.setItem('initials',JSON.stringify(initials))
-    JSON.parse (localStorage.getItem ('initials'))
-    initials.push ({name: 'Andreas'})
+  quizContent.append(newDiv);
+  let userInitials = document.getElementById("initials");
+  let initialSubmit = document.getElementById("initial-submit");
 
-}   
+  initialSubmit.addEventListener("click", function (event) {
+    initials = userInitials.value;
+
+    localStorage.setItem("initials", JSON.stringify(initials));
+
+    newDiv.remove();
+    startButton.setAttribute("style", "display: block");
+
+    // let newButton = document.createElement("button");
+    // newButton.textContent = 'Start Quiz';
+
+    // newButton.addEventListener("click", function (event) {
+    //   newButton.setAttribute("style", "display: none");
+      finalScore.remove();
+    //   timer = document.createElement("h3");
+    //   secondsLeft = 60;
+    //   quizIndex = 0;
+    //   userScore = 0;
+    //   timer.textContent = secondsLeft;
+    //   quizContent.append(timer);
+
+    //   startTimer();
+    //   nextQuestion();
+    // });
+    // quizContent.append(newButton);
+  });
+}
+//   JSON.parse(localStorage.getItem("initials"));
